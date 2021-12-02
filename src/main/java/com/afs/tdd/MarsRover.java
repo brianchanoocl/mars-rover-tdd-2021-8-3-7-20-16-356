0098f1972f02bdd7446ec2ae3bf1b67fbbb3e7c0
brianchanoocl <Delubdi118>
2021-12-02 19:44:11 +0800

    [MarsRover] [Brian] -feat: init project, add constructor

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,13 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int coordinateX;
+    private int coordinateY;
+    private char direction;
+
+    public MarsRover(int coordinateX, int coordinateY, char direction) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.direction = direction;
+    }
+}
