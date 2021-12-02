957256683126a99316afd836ce6c42a6989cafba
brianchanoocl <Delubdi118>
2021-12-02 20:28:07 +0800

    [MarsRover] [Brian] -feat: Complete marsRover funxtions, start test case

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -10,4 +10,68 @@ public class MarsRover {
         this.coordinateY = coordinateY;
         this.direction = direction;
     }
+
+    public String executeCommand(char command){
+        if(command == 'M')
+            move();
+        else
+            turn(command);
+
+        return reportLocationStatus();
+    }
+
+    private String reportLocationStatus() {
+        return String.format("%d %d %c", coordinateX, coordinateY, direction);
+    }
+
+    private void move() {
+        switch (direction) {
+            case 'N':
+                coordinateY++;
+                break;
+            case 'E':
+                coordinateX++;
+                break;
+            case 'S':
+                coordinateY--;
+                break;
+            case 'W':
+                coordinateX--;
+                break;
+        }
+    }
+
+    private void turn(char command) {
+        if(command == 'L'){
+            switch (direction) {
+                case 'N':
+                    direction = 'W';
+                    break;
+                case 'E':
+                    direction = 'N';
+                    break;
+                case 'S':
+                    direction = 'E';
+                    break;
+                case 'W':
+                    direction = 'S';
+                    break;
+            }
+        } else if(command == 'R') {
+            switch (direction) {
+                case 'N':
+                    direction = 'E';
+                    break;
+                case 'E':
+                    direction = 'S';
+                    break;
+                case 'S':
+                    direction = 'W';
+                    break;
+                case 'W':
+                    direction = 'N';
+                    break;
+            }
+        }
+    }
 }
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -3,8 +3,17 @@ package com.afs.tdd;
 import org.junit.jupiter.api.Test;
 
 class DemoTest {
+
     @Test
-    void write_your_first_test() {
+    void should_corrdinateY_change_to_1_when_move_given_command_M_and_direction_N() {
+        // given
+
+        // when
+
+        // return
+    }
+    @Test
+    void should_direction_change_to__() {
 
     }
 }
