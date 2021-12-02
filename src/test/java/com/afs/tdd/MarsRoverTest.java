9259753063db530ce96bc315b99b23d13122f184
brianchanoocl <Delubdi118>
2021-12-02 20:56:23 +0800

    [MarsRover] [Brian] -feat Add MarsRoverTest.java

---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,163 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MarsRoverTest {
+
+    @Test
+    void should_coordinateY_change_to_1_when_move_given_command_M_and_direction_N() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'N');
+        char command = 'M';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 1 N", result);
+    }
+
+    @Test
+    void should_coordinateX_change_to_1_when_move_given_command_M_and_direction_E() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'E');
+        char command = 'M';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("1 0 E", result);
+    }
+
+    @Test
+    void should_coordinateY_change_to_minus_1_when_move_given_command_M_and_direction_S() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'S');
+        char command = 'M';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 -1 S", result);
+    }
+
+    @Test
+    void should_coordinateX_change_to_minus_1_when_move_given_command_M_and_direction_W() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'W');
+        char command = 'M';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("-1 0 W", result);
+    }
+
+    @Test
+    void should_direction_change_to_W_when_turn_given_command_L_and_direction_N() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'N');
+        char command = 'L';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 W", result);
+    }
+
+    @Test
+    void should_direction_change_to_N_when_turn_given_command_L_and_direction_E() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'E');
+        char command = 'L';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 N", result);
+    }
+
+    @Test
+    void should_direction_change_to_E_when_turn_given_command_L_and_direction_S() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'S');
+        char command = 'L';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 E", result);
+    }
+
+    @Test
+    void should_direction_change_to_S_when_turn_given_command_L_and_direction_W() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'W');
+        char command = 'L';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 S", result);
+    }
+
+    @Test
+    void should_direction_change_to_E_when_turn_given_command_R_and_direction_N() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'N');
+        char command = 'R';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 E", result);
+    }
+
+    @Test
+    void should_direction_change_to_S_when_turn_given_command_R_and_direction_E() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'E');
+        char command = 'R';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 S", result);
+    }
+
+    @Test
+    void should_direction_change_to_W_when_turn_given_command_R_and_direction_S() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'S');
+        char command = 'R';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 W", result);
+    }
+
+    @Test
+    void should_direction_change_to_N_when_turn_given_command_R_and_direction_w() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'W');
+        char command = 'R';
+
+        // when
+        String result = marsRover.executeCommand(command);
+
+        // return
+        assertEquals("0 0 N", result);
+    }
+}
